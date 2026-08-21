1498c5d8ba38d3804936f4e299cff1e00a808cef
Flytium-drones <flytium-drones-email@gmail.com>
2026-08-21 13:21:58 +0530

    Fix React unused import to ensure Vercel build succeeds

--- src/pages/VerifyCertificate.jsx
+++ src/pages/VerifyCertificate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Layout from "../components/Layout/Layout";
 import axios from "axios";
 import { API_URL } from "../api";
